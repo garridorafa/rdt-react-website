8a1a46d361e7b790be69a78c21e83f7adb5e06af
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2021-05-02 03:40:46 -0400

    Add Home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../../App.css";
+import Hero from "../../components/Hero";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
