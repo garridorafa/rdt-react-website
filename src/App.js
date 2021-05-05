fcc88196d4531c428c920c1b67881041825e19b5
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2021-05-05 13:56:26 -0400

    fix signUp route

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/services" component={Services} />
           <Route path="/products" component={Products} />
-          <Route path="/sing-up" component={SignUp} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
