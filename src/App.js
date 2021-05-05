8491dd9eb858d305023eaeb6058dcd90a5310a81
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2021-05-05 13:48:43 -0400

    Adds some examples pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import Services from "./pages/services";
+import Products from "./pages/Products";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sing-up" component={SignUp} />
         </Switch>
       </Router>
     </>
