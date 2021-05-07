0cadc2a4915dcd80f76299b8516c50605885d13b
Rafa Garrido <rafagarridolara@gmail.com>
2021-05-07 18:47:18 -0400

    Add deploy with gh-pages

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -7,7 +7,7 @@ function CardItem({ path, label, src, text }) {
       <li className="cards__item">
         <Link className="cards__item__link" to={path}>
           <figure className="cards__item__pic-wrap" data-category={label}>
-            <img src={src} alt="Travel Image" className="cards__item__img" />
+            <img src={src} alt="Travel example" className="cards__item__img" />
           </figure>
           <div className="cards__item__info">
             <h5 className="cards__item__text">{text}</h5>
