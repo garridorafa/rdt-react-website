686faf939e33287c91c8f969a3ee91c1c072d25a
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2021-05-07 17:59:55 -0400

    Creacte Layout component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Services from "./pages/services";
 import Products from "./pages/Products";
 import SignUp from "./pages/SignUp";
-import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/services" component={Services} />
-          <Route path="/products" component={Products} />
-          <Route path="/sign-up" component={SignUp} />
-        </Switch>
-        <Footer />
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/services" component={Services} />
+            <Route path="/products" component={Products} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </Layout>
       </Router>
     </>
   );
